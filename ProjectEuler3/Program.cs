5565893a8ac78e544ccf916345012754c7ba9a40
mgh3326 <mgh3326@naver.com>
2018-01-19 23:10:32 +0900

    complete

--- ProjectEuler3/Program.cs
+++ ProjectEuler3/Program.cs
@@ -2,11 +2,32 @@
 
 namespace ProjectEuler3
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static bool NumOfPrime(int num)//이게 왜 static?
         {
-            Console.WriteLine("Hello World!");
+            for (var i = 1; i < num/2; i++)//절반 까지만 하면 되는건가?
+            {
+                if (num % (i+1) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static void Main(string[] args)
+        {
+            const long num = 600851475143;
+            for (var i = 1; i < Math.Sqrt(num); i++)
+            {
+                if (num % (i + 1) != 0) continue;
+                if (NumOfPrime(i + 1))
+                {
+                    Console.WriteLine(i + 1);
+                }
+
+            }
         }
     }
 }
